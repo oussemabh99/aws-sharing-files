0eae8a3d02381d9bd39ffdbf7efc1b3f2bb62930
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2025-10-13 11:40:50 +0100

    0.0.2

--- auth-server/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__)
-    from app.auth.routes import auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth') 
-    return app
